902aafe24947c7933b397a2c00d772185288ef2b
Kamil <kamil@copper.io>
2014-11-14 15:25:50 +0100

    Ugly patch for double slash

--- lib/omniauth/strategy.rb
+++ lib/omniauth/strategy.rb
@@ -246,7 +246,7 @@ module OmniAuth
     end
 
     def on_path?(path)
-      current_path.casecmp(path) == 0
+      current_path.gsub(/^\/\//, '/').casecmp(path) == 0
     end
 
     def options_request?
